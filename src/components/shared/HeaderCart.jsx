bbaddbaf22e6d0069f97388ca5158f958b5a653a
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-21 00:30:18 +0430

    debug header cart

--- src/components/shared/HeaderCart.jsx
+++ src/components/shared/HeaderCart.jsx
@@ -1,15 +1,14 @@
 import Image from 'next/image';
 import React from 'react';
 import Link from 'next/link';
-
 const HeaderCart = () => {
   return (
     <>
-      <main className=' h-full m-0 p-0 w-full'>
+      <main className='h-screen lg:h-full m-0 p-0 w-full md:max-h-96 md:max-w-4xl md:mx-auto'>
         {/* Cart header start */}
         <article className='w-full h-48 md:max-w-6xl md:mx-auto md:max-h-full md:h-full md:flex'>
           {/* cart Image */}
-          <div className='block w-full m-0 md:h-full md:m-0'>
+          <div className='block w-full m-0  md:m-0'>
             <Image
               className='w-full object-cover md:h-full md:w-full md:m-0'
               src={'/images/header.webp'}
@@ -20,19 +19,19 @@ const HeaderCart = () => {
           </div>
           {/* cart Image */}
           {/* cart Info */}
-          <div className='w-full mx-auto bg-mainLightColor md:w-1/2 md:min-h-full md:flex md:items-center'>
+          <div className='w-full mx-auto bg-mainLightColor md:w-1/2 md:flex md:items-center'>
             <div className='w-10/12 mx-auto flex flex-col items-center justify-center'>
               <h3 className='text-secondaryLightColor mt-7 md:mt-2'>
                 WELCOME TO YOUR FUTURE
               </h3>
               <div className='text-center my-5 md:my-1'>
-                <p className='font-light tracking-wide text-3xl text-mainColor'>
+                <p className='font-light tracking-wide text-3xl lg:text-3xl lg:font-bold text-mainColor'>
                   Freedom{' '}
                 </p>
-                <p className='font-light tracking-wide text-3xl text-mainColor'>
+                <p className='font-light tracking-wide text-3xl lg:text-3xl lg:font-bold text-mainColor'>
                   Security{' '}
                 </p>
-                <p className='font-light tracking-wide text-3xl text-mainColor'>
+                <p className='font-light tracking-wide text-3xl lg:text-3xl lg:font-bold text-mainColor'>
                   Happiness
                 </p>
                 <p className='my-2 text-secondaryLightColor'>
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,11 +13,13 @@ const HomePage = () => {
       <HeaderCart />
 
       {/* Info */}
-      <div className='text-center my-20 md:m-10'>
+      <div className='text-center my-20 md:mx-52 h-screen'>
         {/* Info one */}
         <div className='my-4'>
-          <h3 className='text-mainLightColor text-xl py-2'>BUILT ON 30 YEARS OF TRUST</h3>
-          <p className='font-light tracking-wide text-sm md:text-lg'>
+          <h3 className='text-mainLightColor text-xl py-2'>
+            BUILT ON 30 YEARS OF TRUST
+          </h3>
+          <p className='font-light tracking-wide text-sm md:text-lg md:p-2'>
             With more than 30 years of experience in assisting our clients
             reaching their immigration and business set up goals in Dubai, we
             now have expanded our services internationally to include
@@ -28,8 +30,10 @@ const HomePage = () => {
 
         {/* Info two */}
         <div>
-          <h3 className='text-mainLightColor text-xl py-2'>PROFESSIONALISM IS OUR MOTTO</h3>
-          <p className='font-light tracking-wide text-sm md:text-lg'>
+          <h3 className='text-mainLightColor text-xl py-2'>
+            PROFESSIONALISM IS OUR MOTTO
+          </h3>
+          <p className='font-light tracking-wide text-sm md:text-lg md:p-2'>
             We are partnered with Authorized CBIU Agents that have a great deal
             of experience in handling citizenship by investment applications. We
             provide you with professional guidance on preparing and submitting
@@ -41,8 +45,10 @@ const HomePage = () => {
 
         {/* Info three */}
         <div>
-          <h3 className='text-mainLightColor text-xl py-2'>WE ARE BY YOUR SIDE ALL ALONG</h3>
-          <p className='font-light tracking-wide text-sm md:text-lg'>
+          <h3 className='text-mainLightColor text-xl py-2'>
+            WE ARE BY YOUR SIDE ALL ALONG
+          </h3>
+          <p className='font-light tracking-wide text-sm md:text-lg md:p-2'>
             The first step is for us to work with you to find out exactly what
             you need. Through our understanding of the law and applying it to
             your circumstances, we will craft a path to success for you to
