25e5cc6161737dad889b3c8a3872213d46ed12c5
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-22 01:55:16 +0430

    create notFound page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div className='text-center'>
+      <h1>Sorry! it seem's your lost :(</h1>
+    </div>
+  );
+};
+
+export default NotFoundPage;
