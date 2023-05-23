8ff93d22c44eaa3f11f4c66403a6fb2bc5124281
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-24 00:40:33 +0430

    add faveicons to _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
+      <link
+        rel='icon'
+        type='image/png'
+        href='/faveicons/tab-icon-32x32.png'
+        sizes='32x32'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        href='/faveicons/tab-icon-16x16.png'
+        sizes='16x16'
+      />
+
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
