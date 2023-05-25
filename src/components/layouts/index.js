932899c10f6a0b17299bac69ae705d675224527b
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-05-26 00:47:07 +0430

    add padding to layout

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,6 +1,6 @@
 const Layout = ({ children }) => {
   return (
-    <main className=' min-h-screen max-w-full m-3  md:max-w-5xl   md:mx-auto'>
+    <main className=' min-h-screen max-w-full m-3 px-4  md:max-w-5xl   md:mx-auto'>
       {children}
     </main>
   );
