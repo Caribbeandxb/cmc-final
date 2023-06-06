e91a02cfd1116250dd6d10f22cccb6a2c74067c2
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-06 21:53:56 +0430

    add two colmuns

--- src/components/shared/Countries.jsx
+++ src/components/shared/Countries.jsx
@@ -3,7 +3,7 @@ const Countries = ({ rtl = 'left' }) => {
   // render all countries for dominica-passport
   return (
     <>
-      <ul className={`my-3 w-full text-sm grid grid-cols-2 lg:grid-cols-3 text-${rtl} list-decimal font-normal text-slate-600`}>
+      <ul className={`my-3 w-full text-sm grid grid-cols-2 text-${rtl} list-decimal font-normal text-slate-600`}>
         {countries.map((country,idx) => (
           <li className={`w-fit mx-6 my-2`} key={country}>
             {country}
