1cd170136eb456a3c3a80c9160f9c8c5cee95732
parham mosadeqzadeh <pmosadeqzadeh@gmail.com>
2023-06-06 15:19:05 +0430

    add developer

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,11 +2,12 @@ const Footer = () => {
   const currentYear = new Date().getFullYear().toString();
   return (
     <footer className='text-center font-light mt-9 mb-3'>
-      <p>
+      <p className='text-lg px-3'>
         COPYRIGHT &copy; {currentYear} Caribbean Management Consultancies - ALL
         RIGHTS RESERVED.
       </p>
       {/* add developer name and information */}
+      <span className='capitalize font-light text-secondaryColor hover:text-mainLightColor transition-colors duration-300'><a href="mailto:pmosadeqzadeh@gmail.com">develped by parham mosadeqzadeh</a></span>
     </footer>
   );
 };
